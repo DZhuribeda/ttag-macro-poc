59dd9e214f9f968b0c7d04980c66f98e2b401375
DZhuribeda <dmitry.zhurybida@datarobot.com>
2018-11-01 19:03:29 +0200

    use static field

--- examples/cra/src/Translations.js
+++ examples/cra/src/Translations.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { t, jt, gettext, ngettext, msgid } from 'ttag.macro';
 
 const a = 'text';
@@ -8,28 +9,36 @@ const PluralText = ({ n }) => (
   ngettext(msgid`plural form with ${ n } plural`, `plural form with ${ n } plurals`, n)
 );
 
-const Translations = () => (
-  <>
-    {t`simple string literal`}
-    <br />
-    {jt`${ a } simple string ${ b } literal with formatting`}
-    <br />
-    {t`no translator notes`}
-    <br />
-    <PluralText n={1} />
-    <br />
-    <PluralText n={2} />
-    <br />
-    {t`
-      first line
-      second line
-      third line
-    `}
-    <br />
-    {gettext`${ a } spaces test`}
-    <br />
-    {t`{name} fuzzy name`}
-  </>
-);
+class Translations extends React.Component {
+  static propTypes = {
+    test: PropTypes.any,
+  };
+
+  render() {
+    return (
+      <>
+        {t`simple string literal`}
+        <br />
+        {jt`${ a } simple string ${ b } literal with formatting`}
+        <br />
+        {t`no translator notes`}
+        <br />
+        <PluralText n={1} />
+        <br />
+        <PluralText n={2} />
+        <br />
+        {t`
+          first line
+          second line
+          third line
+        `}
+        <br />
+        {gettext`${ a } spaces test`}
+        <br />
+        {t`{name} fuzzy name`}
+      </>
+    );
+  }
+};
 
 export default Translations;
--- examples/cra/src/index.js
+++ examples/cra/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { addLocale, useLocale } from 'ttag.macro';
+import { addLocale, useLocale } from 'ttag';
 
 // Load translations in develop mode
 if (process.env.NODE_ENV === 'development') {
